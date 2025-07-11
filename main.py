9920e356124944fa92267a765517f14b2d324f79
Ethan <43587522+universestrike@users.noreply.github.com>
2025-07-11 00:58:28 +0000

    Adding main.py and creating frankenstein read

---
+++ main.py
@@ -0,0 +1,14 @@
+def get_book_text(filepath):
+    with open(filepath, 'r') as f:
+        return f.read()
+
+
+def main():
+
+    filepath = 'books/frankenstein.txt'
+    text = get_book_text(filepath)
+    print(text)
+
+
+if __name__ == '__main__':
+    main()
