90d7a638e59850f5f55c9fb2711680ad75540f7d
Ethan <43587522+universestrike@users.noreply.github.com>
2025-07-11 19:40:09 +0000

    added pretty print report to main

--- main.py
+++ main.py
@@ -5,14 +5,21 @@ def get_book_text(filepath):
     with open(filepath, 'r') as f:
         return f.read()
 
-def main():
+def pretty_print(filepath, words, chars):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {words} total words")
+    print("--------- Character Count -------")
+    for char in sorted(chars):
+        print(f"{char}: {chars[char]}")
 
+def main():
     filepath = 'books/frankenstein.txt'
     text = get_book_text(filepath)
     words = get_word_count(text)
     chars = get_char_count(text)
-    for char in sorted(chars):
-        print(f"'{char}': {chars[char]}")
+    pretty_print(filepath, words, chars)
 
 
 if __name__ == '__main__':
