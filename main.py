2b1f1c684aefbeda89b95965005980a0b7345c04
Ethan <43587522+universestrike@users.noreply.github.com>
2025-07-11 19:27:53 +0000

    count characters lowercase and alphebetize

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_word_count
+from stats import get_char_count
 
 def get_book_text(filepath):
     with open(filepath, 'r') as f:
@@ -9,7 +10,9 @@ def main():
     filepath = 'books/frankenstein.txt'
     text = get_book_text(filepath)
     words = get_word_count(text)
-    print(f"{words} words found in the document")
+    chars = get_char_count(text)
+    for char in sorted(chars):
+        print(f"'{char}': {chars[char]}")
 
 
 if __name__ == '__main__':
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def get_word_count(text): 
     wordCount = text.split()
     return len(wordCount)
+
+def get_char_count(text):
+    char_count = {}
+    text = text.lower()
+    for char in text:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
