a43ef3ea650e32e115bbc8393a4d0407388d46e4
Ethan <43587522+universestrike@users.noreply.github.com>
2025-07-11 01:11:01 +0000

    refactor word count to seperate file

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+from stats import get_word_count
+
 def get_book_text(filepath):
     with open(filepath, 'r') as f:
         return f.read()
-
 
 def main():
 
     filepath = 'books/frankenstein.txt'
     text = get_book_text(filepath)
-    print(text)
+    words = get_word_count(text)
+    print(f"{words} words found in the document")
 
 
 if __name__ == '__main__':
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_word_count(text): 
+    wordCount = text.split()
+    return len(wordCount)
